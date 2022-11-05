8b73bbd161b9bb921c4ab404579c92319bd0e8ce
Dianakariuki <dianakariuki842@gmail.com>
2022-11-05 12:02:18 +0300

    Show the loading animation

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 document.querySelector('#loan-form').addEventListener('submit', function(e){
      // Hide the results
      document.querySelector('#results').style.display = 'none';
+       // Show the loading animation
+    document.querySelector('#loading').style.display = 'block';
