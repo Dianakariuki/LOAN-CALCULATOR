eea98a99b0a5f224fa1b9b6f8204c7853cfd9dde
Dianakariuki <dianakariuki842@gmail.com>
2022-11-05 12:01:40 +0300

    feature: hide the resuts

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 // Submit event listener
 document.querySelector('#loan-form').addEventListener('submit', function(e){
+     // Hide the results
+     document.querySelector('#results').style.display = 'none';
