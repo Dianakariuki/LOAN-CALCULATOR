bd7cbf57059602e1cbe0f35da43a97ca20fdc7c1
Dianakariuki <dianakariuki842@gmail.com>
2022-11-07 15:34:47 +0300

    ft: calculate parsefloat

--- index.js
+++ index.js
@@ -18,5 +18,10 @@ function calculateResults(){
   const monthlyPayment = document.getElementById(monthly-payment);
   const totalPayment = document.getElementById(total-payment);
   const totalInterest = document.getElementById(total-interest);
+
+
   
+  const principle = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
 }
