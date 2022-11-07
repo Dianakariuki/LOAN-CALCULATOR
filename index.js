2c84a54c05ea111b2e0faa81921d23af09d9b0f3
Dianakariuki <dianakariuki842@gmail.com>
2022-11-07 15:34:08 +0300

    ft: payment and interest variables

--- index.js
+++ index.js
@@ -15,5 +15,8 @@ function calculateResults(){
   const amount = document.getElementById('amount');
   const interest = document.getElementById(interest);
   const years = document.getElementById(years);
+  const monthlyPayment = document.getElementById(monthly-payment);
+  const totalPayment = document.getElementById(total-payment);
+  const totalInterest = document.getElementById(total-interest);
   
 }
