28a27d25ae60ea541e13790867c060f0bb5fccf2
Dianakariuki <dianakariuki842@gmail.com>
2022-11-14 09:14:15 +0300

    new js

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
+
 // Submit event listener
 document.querySelector('#loan-form').addEventListener('submit', function(e){
-     // Hide the results
-     document.querySelector('#results').style.display = 'none';
-       // Show the loading animation
-    document.querySelector('#loading').style.display = 'block';
-    setTimeout(calculateResults, 2000);
+   // Hide the results
+   document.querySelector('#results').style.display = 'none';
+   // Show the loading animation
+   document.querySelector('#loading').style.display = 'block';
 
-    e.preventDefault();
+   setTimeout(calculateResults, 2000);
+
+   e.preventDefault();
 });
 
-/// Calculate the results
+// Calculate the results
 function calculateResults(){
-  // Declare UI variables
-  const amount = document.getElementById('amount');
-  const interest = document.getElementById(interest);
-  const years = document.getElementById(years);
-  const monthlyPayment = document.getElementById(monthly-payment);
-  const totalPayment = document.getElementById(total-payment);
-  const totalInterest = document.getElementById(total-interest);
+   // Declare UI variables
+   const amount = document.getElementById('amount');
+   const interest = document.getElementById('interest');
+   const years = document.getElementById('years');
+   const monthlyPayment = document.getElementById('monthly-payment');
+   const totalPayment = document.getElementById('total-payment');
+   const totalInterest = document.getElementById('total-interest');
 
-
-  
-  const principle = parseFloat(amount.value);
-  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-  const calculatedPayments = parseFloat(years.value) * 12;
-
+   const principle = parseFloat(amount.value);
+   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+   const calculatedPayments = parseFloat(years.value) * 12;
+   
    // Calculate monthly payments
    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
    const monthly = (principle * x * calculatedInterest) / (x - 1);
 
    if(isFinite(monthly)){
-    monthlyPayment.value = monthly.toFixed(2);
-    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments) - principle).toFixed(2);
+       monthlyPayment.value = monthly.toFixed(2);
+       totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+       totalInterest.value = ((monthly * calculatedPayments) - principle).toFixed(2);
 
        // Show results and hide the loader
        document.querySelector('#results').style.display = 'block';
@@ -41,14 +41,13 @@ function calculateResults(){
        // Show an error message
        showError('Please check your inputs');
    }
-
 }
+
 // Show error on invalid input
 function showError(error){
-  // Hide the results and loader
-  document.querySelector('#results').style.display = 'none';
-  document.querySelector('#loading').style.display = 'none';
-
+   // Hide the results and loader
+   document.querySelector('#results').style.display = 'none';
+   document.querySelector('#loading').style.display = 'none';
 
    // Create a div for the error message
    const errorDiv = document.createElement('div');
